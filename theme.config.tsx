11a62e80112a78f9988c00aa8b7835a84f063bdc
Grav <75702884+Gravxd@users.noreply.github.com>
2025-02-02 08:26:12 +0000

    first stuff

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <Image src="https://grav.wtf/chromaroleicondiscord.png" alt="logo" width={32} height={32} />,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/Gravxd",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.gg/chromalabs",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    content: 'Nextra Docs Template',
+    content: "Nextra Docs Template",
   },
-}
+};
 
-export default config
+export default config;
