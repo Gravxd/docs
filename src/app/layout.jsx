94bc00e7d56b7cd7219dd0b4a1006cd5802469d5
blaaaak <103514165+blaaaak@users.noreply.github.com>
2025-02-02 22:10:51 +1100

    Fixes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,20 +9,21 @@ import "nextra-theme-docs/style.css";
 export const metadata = {
   metadataBase: new URL("https://nextra.site"),
   title: {
-    template: "%s - Nextra",
+    template: "%s - ChromaLabs",
   },
-  description: "Nextra: the Next.js site builder",
-  applicationName: "Nextra",
+  description: "ChromaLabs: FiveM Scripts & Resources",
+  applicationName: "ChromaLabs",
   generator: "Next.js",
   appleWebApp: {
-    title: "Nextra",
+    title: "ChromaLabs",
   },
+
   other: {
     "msapplication-TileImage": "/ms-icon-144x144.png",
     "msapplication-TileColor": "#fff",
   },
   twitter: {
-    site: "https://nextra.site",
+    site: "https://discord.gg/chromalabs",
   },
 };
 
@@ -44,9 +45,9 @@ export default async function RootLayout({ children }) {
             <Banner storageKey="welcometothedocs">Welcome to the Docs</Banner>
           }
           navbar={navbar}
-          footer={<Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>}
-          // editLink="Edit this page on GitHub"
-          // docsRepositoryBase="https://github.com/shuding/nextra/blob/main/examples/docs"
+          footer={<Footer>{new Date().getFullYear()} © ChromaLabs.</Footer>}
+          editLink="Edit this page on GitHub"
+          docsRepositoryBase="https://github.com/Gravxd/docs"
           sidebar={{ defaultMenuCollapseLevel: 1 }}
           pageMap={await getPageMap()}
         >
--- src/content/_meta.js
+++ src/content/_meta.js
@@ -1,4 +1,4 @@
 export default {
-  fivem: "FiveM",
   index: "",
+  fivem: "FiveM",
 };
