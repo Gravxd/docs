3c6f2b85b6e0840d02f1004c270d19d8093219e2
Grav <75702884+Gravxd@users.noreply.github.com>
2025-02-02 08:45:20 +0000

    base layout

--- pages/_meta.tsx
+++ pages/_meta.tsx
@@ -1,15 +1,5 @@
 export default {
   index: "Introduction",
-  another: "Another Page",
-  advanced: "Advanced (A Folder)",
-  about: {
-    title: "About",
-    type: "page"
-  },
-  contact: {
-    title: "Contact ↗",
-    type: "page",
-    href: "https://twitter.com/shuding_",
-    newWindow: true
-  }
+  vmenuox: "vMenu-ox",
+  fivem: "FiveM",
 }
